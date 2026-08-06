19e230593fb2902f27c7832e8aef024de1ad7949
Kuba <kuba4224@gmail.com>
2026-08-06 16:49:00 +0200

    Sprzatanie folderow diagnostycznych, wykluczenie ich z tsconfig

--- _MOZESZ_USUNAC_folder_diagnostyczny/_diag_new_app/index_leftover_removed_from_app.tsx
+++
@@ -1,8 +0,0 @@
-import { View, Text } from 'react-native';
-export default function Index() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ color: '#fff' }}>index</Text>
-    </View>
-  );
-}
